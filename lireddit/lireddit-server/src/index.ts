d02291f9cf042ed0d887a8fb59705042919371d2
dannywuwu <d246wu@gmail.com>
2020-08-22 21:31:22 -0400

    mikroORM migrations

--- lireddit/lireddit-server/src/index.ts
+++ lireddit/lireddit-server/src/index.ts
@@ -5,7 +5,9 @@ import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
 
 const main = async () => {
+  // connect to db
   const orm = await MikroORM.init(microConfig);
+  // run migrations
   await orm.getMigrator().up();
   // const post = orm.em.create(Post, { title: "my first post" });
   // await orm.em.persistAndFlush(post);
