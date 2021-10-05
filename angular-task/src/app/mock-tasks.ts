c3a76c38fc257a5f23e18a9f69998d4bd8754424
dannywuwu <d246wu@gmail.com>
2021-10-05 13:12:31 -0400

    angular tasks - ngFor, ts mock tasks

---
+++ angular-task/src/app/mock-tasks.ts
@@ -0,0 +1,21 @@
+import { Task } from './Task';
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'beat big tree',
+    day: 'yesterday',
+    reminder: false,
+  },
+  {
+    id: 2,
+    text: 'dodge big arrows',
+    day: 'yesterday',
+    reminder: false,
+  },
+  {
+    id: 3,
+    text: 'defeat lgt',
+    day: 'tomorrow',
+    reminder: true,
+  },
+];
